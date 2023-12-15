2f91e0ada518a0b158830f07f733e3ab9d55fc54
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-12-15 09:46:06 +0500

    update

--- src/components/SectionSeven/SectionSeven.vue
+++ src/components/SectionSeven/SectionSeven.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="faq" class="px-4 lg:px-0 xl:px-4 2xl:px-0 mt-[80px] md:mt-[100px] xl:mt-[200px]">
         <div class="container mx-auto ">
-            <h1 class="text-[27px] text-[#424259] font-[700] xl:text-[36px] pt-[48px] font-rubik">Bizning mijozlarimiz fikrlarimiz</h1>
+            <h1 class="text-[27px] text-[#424259] font-[700] xl:text-[36px] pt-[48px] font-rubik">Bizning mijozlarimiz fikrlari</h1>
             <p class="text-[16px] text-[#424259] font-[400] mt-[16px] font-rubik">Bizning mijozlarimiz fikrlari bizning xizmatlarimiz va shifokorlar xizmatlari haqida so’zlaydi. Xizmatlarimizdan foydalangan har bir odam mamnun va xursand.</p>
         </div>
         <CardThree class="mt-[40px] md:mt-[48px] xl:mt-[64px]"/>
